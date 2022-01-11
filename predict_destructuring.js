19213a96dc7ebd51c8c2a2e8fd6557e39ad8fda6
samdhein <hein.sam@gmail.com>
2022-01-10 18:36:01 -0800

    completed assignment

---
+++ predict_destructuring.js
@@ -0,0 +1,74 @@
+// Problem 1
+const cars = ['Tesla', 'Mercedes', 'Honda']
+const [ randomCar ] = cars
+const [ ,otherRandomCar ] = cars
+//Predict the output
+console.log(randomCar)
+console.log(otherRandomCar)
+
+// Predicted output: Tesla, Mercedes
+// Correct
+
+// Problem 2
+const employee = {
+    name: 'Elon',
+    age: 47,
+    company: 'Tesla'
+}
+const { name: otherName } = employee;
+//Predict the output
+console.log(otherName);
+
+// Predicted output: undefined, Elon
+// Sort of correct--didn't run because "name is not defined" error
+
+// Problem 3
+
+const person = {
+    name: 'Phil Smith',
+    age: 47,
+    height: '6 feet'
+}
+const password = '12345';
+const { password: hashedPassword } = person;  
+//Predict the output
+console.log(password);
+console.log(hashedPassword);
+
+// Predicted output: 12345, undefined (person has no password attribute)
+// Correct
+
+// Problem 4
+
+const numbers = [8, 2, 3, 5, 6, 1, 67, 12, 2];
+const [,first] = numbers;
+const [,,,second] = numbers;
+const [,,,,,,,,third] = numbers;
+//Predict the output
+console.log(first == second);
+console.log(first == third);
+
+// Predicted output: error, can't reassign const
+// Incorrect: these are booleans 
+// Second try: false, true (correct)
+
+
+// Problem 5
+
+const lastTest = {
+    key: 'value',
+    secondKey: [1, 5, 1, 8, 3, 3]
+}
+const { key } = lastTest;
+const { secondKey } = lastTest;
+const [ ,willThisWork] = secondKey;
+//Predict the output
+console.log(key);
+console.log(secondKey);
+console.log(secondKey[0]);
+console.log(willThisWork);
+
+// Predicted Output: value, type error (array not object), would be 1 but type error
+// would be 5 but type error
+
+// Correct? Predictions were correct but {} were the correct choice for { secondkey } ln 63
